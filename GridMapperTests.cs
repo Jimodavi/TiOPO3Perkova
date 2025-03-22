1a3d65019ca803364e06e63a70f89181e29488dd
Liza <shovel.f@list.ru>
2025-03-22 18:20:29 +0700

    Добавлен тест контруктора GridMapper

---
+++ GridMapperTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace lr3_2.Tests
+{
+    [TestClass()]
+    class GridMapperTests
+    {
+        [TestMethod()]
+        public void GridMapperCreationTest()
+        {
+            GridMapper actual = new GridMapper();
+            Assert.IsNotNull(actual, "Сбой создания объекта GridMapper. Ожидается not null объект класса.");
+        }
+    }
+}
