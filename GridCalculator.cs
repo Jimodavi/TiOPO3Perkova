c617324d09387f7dd6bcecec2a6484bce7129d66
Liza <shovel.f@list.ru>
2025-03-22 17:49:18 +0700

    Добавлен тривиальный GridCalculator.CalculateGrid

--- GridCalculator.cs
+++ GridCalculator.cs
@@ -5,5 +5,13 @@
         public GridCalculator()
         {
         }
+
+        public void CalculateGrid(Grid grid)
+        {
+            grid[0, 0] = 1; grid[0, 1] = Grid.mine_value; grid[0, 2] = 1;
+            grid[1, 1] = 1; grid[1, 5] = 1;
+            grid[2, 4] = 1; grid[2, 5] = Grid.mine_value; grid[2, 6] = 1;
+            grid[3, 5] = 1;
+        }
     }
 }
