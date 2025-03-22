673eb4260269890e7dc2fba7ec66b39f9948c6ed
Liza <shovel.f@list.ru>
2025-03-22 15:48:37 +0700

    Добавлен тест контруктора Grid

---
+++ GridTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace lr3_2.Tests
+{
+    [TestClass()]
+    public class GridTests
+    {
+        [TestMethod()]
+        public void GridCreationTest()
+        {
+            int expected_rows = 6;
+            int expected_columns = 6;
+            Grid actual = new Grid(expected_rows, expected_columns);
+            Assert.IsNotNull(actual, "Сбой создания сетки. Ожидается not null объект класса.");
+        }
+    }
+}
