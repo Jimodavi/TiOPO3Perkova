d0db751df2e3915afa95ae61b5316f15c1755ae9
Liza <shovel.f@list.ru>
2025-03-22 19:35:25 +0700

    Добавлен тест GridMapper.ToInt('9')

--- GridMapperTests.cs
+++ GridMapperTests.cs
@@ -4,13 +4,29 @@ using System;
 namespace lr3_2.Tests
 {
     [TestClass()]
-    class GridMapperTests
+    public class GridMapperTests
     {
         [TestMethod()]
         public void GridMapperCreationTest()
         {
             GridMapper actual = new GridMapper();
             Assert.IsNotNull(actual, "Сбой создания объекта GridMapper. Ожидается not null объект класса.");
+        }
+
+        [TestMethod()]
+        public void GridMapper9ToInt()
+        {
+            int expected = 9;
+            int actual = GridMapper.ToInt('9');
+
+            try
+            {
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception)
+            {
+                throw new AssertFailedException("Сбой конверсии char в int. Ожидается " + expected + ". Фактически " + actual + " .");
+            }
         }
     }
 }
