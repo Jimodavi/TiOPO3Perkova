b0dd90a7b191e22bd2aa28a826f451f4752408f3
Liza <shovel.f@list.ru>
2025-03-22 19:39:51 +0700

    Добавлен тест GridMapper.ToInt('.')

--- GridMapperTests.cs
+++ GridMapperTests.cs
@@ -28,5 +28,21 @@ namespace lr3_2.Tests
                 throw new AssertFailedException("Сбой конверсии char в int. Ожидается " + expected + ". Фактически " + actual + " .");
             }
         }
+
+        [TestMethod()]
+        public void GridMapperDotToInt()
+        {
+            int expected = 0;
+            int actual = GridMapper.ToInt('.');
+
+            try
+            {
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception)
+            {
+                throw new AssertFailedException("Сбой конверсии char в int. Ожидается " + expected + ". Фактически " + actual + " .");
+            }
+        }
     }
 }
