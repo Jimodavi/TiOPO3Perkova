5cbc54403a293d4b5706142afa2555d2e7da6d39
Liza <shovel.f@list.ru>
2025-03-22 17:15:23 +0700

    Добавлен тест контруктора GridCalculator

---
+++ GridCalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace lr3_2.Tests
+{
+    [TestClass()]
+    class GridCalculatorTests
+    {
+        [TestMethod()]
+        public void GridCalculatorCreationTest()
+        {
+            GridCalculator actual = new GridCalculator();
+            Assert.IsNotNull(actual, "Сбой создания объекта GridCalculator. Ожидается not null объект класса.");
+        }
+    }
+}
