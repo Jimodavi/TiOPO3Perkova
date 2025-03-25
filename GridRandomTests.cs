45e6a6ccdc5116b4932740e0c5e2bf8768599e2a
Liza <shovel.f@list.ru>
2025-03-25 19:57:21 +0700

    Добавлен тест GridRandom.CreateRandomGrid(7, 8, 1)

--- GridRandomTests.cs
+++ GridRandomTests.cs
@@ -12,5 +12,27 @@ namespace lr3_2.Tests
             GridRandom actual = new GridRandom();
             Assert.IsNotNull(actual, "Сбой создания объекта GridRandom. Ожидается not null объект класса.");
         }
+
+        [TestMethod()]
+        public void GridRandomCreateRandomGrid1MineTest()
+        {
+            int expected = 1;
+            int rows = 7;
+            int columns = 8;
+            Grid grid = GridRandom.CreateRandomGrid(rows, columns, expected);
+            int actual = 0;
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    if (grid[i, j] == Grid.mine_value) actual += 1;
+
+            try
+            {
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception)
+            {
+                throw new AssertFailedException("Сбой создания радномной Grid. Ожидается " + expected + " мина. Фактически " + actual + " мина.");
+            }
+        }
     }
 }
