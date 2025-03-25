ebe9a512d8bdebab06487d5514a6199feca61b72
Liza <shovel.f@list.ru>
2025-03-25 17:14:24 +0700

    Добавлен тривиальный конструктор ConsoleGame

---
+++ ConsoleGame.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using System.Linq;
+using System;
+
+namespace lr3_2
+{
+    public class ConsoleGame
+    {
+        public ConsoleGame(Grid grid)
+        {
+        }
+    }
+}
