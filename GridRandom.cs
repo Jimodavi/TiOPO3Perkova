3628d3850233a00e11712ed0fddd9ebeb6191599
Liza <shovel.f@list.ru>
2025-03-25 19:58:04 +0700

    Добавлен тривиальный GridRandom.CreateRandomGrid

--- GridRandom.cs
+++ GridRandom.cs
@@ -2,5 +2,13 @@
 {
     public class GridRandom
     {
+        public static Grid CreateRandomGrid(int rows, int columns, int mines)
+        {
+            // todo реализовать рандомное расположение мин
+            Grid grid = new Grid(rows, columns);
+            grid[0, 2] = Grid.mine_value;
+            new GridCalculator().CalculateGrid(grid);
+            return grid;
+        }
     }
 }
