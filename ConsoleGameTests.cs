096f02717fb529c1a042d5a2115fd85576198597
Liza <shovel.f@list.ru>
2025-03-25 17:16:00 +0700

    Добавлен тест ConsoleGame.GridToString() 3 на 2

--- ConsoleGameTests.cs
+++ ConsoleGameTests.cs
@@ -13,5 +13,28 @@ namespace lr3_2.Tests
             ConsoleGame actual = new ConsoleGame(new Grid(1, 1));
             Assert.IsNotNull(actual, "Сбой создания объекта ConsoleGame. Ожидается not null объект класса.");
         }
+
+        [TestMethod()]
+        public void ConsoleGameGridToString3by2Test()
+        {
+            string expected = "|??\n|??\n|??\n";
+            int rows = 3;
+            int columns = 2;
+            Grid grid = new Grid(rows, columns);
+            grid[0, 0] = 2; grid[0, 1] = 6;
+            grid[1, 0] = 9; grid[1, 1] = 6;
+            grid[2, 0] = 1; grid[2, 1] = 1;
+            ConsoleGame game = new ConsoleGame(grid);
+            string actual = game.GridToString();
+
+            try
+            {
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception)
+            {
+                throw new AssertFailedException("Сбой конверсии Grid в string. Ожидается: " + expected + "Фактически: " + actual);
+            }
+        }
     }
 }
