b189fac1e893c601748a05b4bfdc999769d875e1
Liza <shovel.f@list.ru>
2025-03-25 19:36:47 +0700

    Добавлен тест создания GridRandom

---
+++ GridRandomTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace lr3_2.Tests
+{
+    [TestClass()]
+    public class GridRandomTests
+    {
+        [TestMethod()]
+        public void GridRandomCreationTest()
+        {
+            GridRandom actual = new GridRandom();
+            Assert.IsNotNull(actual, "Сбой создания объекта GridRandom. Ожидается not null объект класса.");
+        }
+    }
+}
