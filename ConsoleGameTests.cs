aa46c96852e112ecc70541957ffb8cdb59af26bb
Liza <shovel.f@list.ru>
2025-03-25 17:13:32 +0700

    Добавлен тест на создание ConsoleGame

---
+++ ConsoleGameTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace lr3_2.Tests
+{
+    [TestClass()]
+    public class ConsoleGameTests
+    {
+        [TestMethod()]
+        public void ConsoleGameCreationTest()
+        {
+            ConsoleGame actual = new ConsoleGame(new Grid(1, 1));
+            Assert.IsNotNull(actual, "Сбой создания объекта ConsoleGame. Ожидается not null объект класса.");
+        }
+    }
+}
