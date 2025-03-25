70cdcfd1076d0012c6155040764ae6d3f6c2e63d
Liza <shovel.f@list.ru>
2025-03-25 20:04:40 +0700

    Рефакторинг GridRandom.CreateRandomGrid для прохождения теста GridRandom.CreateRandomGrid(6, 6, 2)

--- GridRandom.cs
+++ GridRandom.cs
@@ -5,8 +5,10 @@
         public static Grid CreateRandomGrid(int rows, int columns, int mines)
         {
             // todo реализовать рандомное расположение мин
+            // todo реализовать обработку неправильного количества mines
             Grid grid = new Grid(rows, columns);
             grid[0, 2] = Grid.mine_value;
+            if (mines > 1) grid[0,0] = Grid.mine_value;
             new GridCalculator().CalculateGrid(grid);
             return grid;
         }
